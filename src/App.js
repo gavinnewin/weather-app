24c7f97523241a3ec804d6e0c4b2a1630b66bc95
Gavin Nguyen <gavinnguyen2002@gmail.com>
2023-04-23 21:31:42 -0700

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-300 font-bold">Hello World</h1>
+      
+    </div>
+  );
+}
+
+export default App;
