d341d393e21e93d20e0783890d8865d0dc851437
Gavin Nguyen <gavinnguyen2002@gmail.com>
2023-04-26 03:53:56 -0700

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500 font-bold">Hello World</h1>
-      
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-md">
+      <TopButtons />
+      <Inputs />
+
+
     </div>
+
+      
   );
 }
 
