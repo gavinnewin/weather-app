71933ae1b622ac8e960967e1c9d68e2651a3e09a
Gavin Nguyen <gavinn1@uci.edu>
2024-07-15 02:47:58 -0700

    1st push

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './style.css';
+
+let curX = 0;
+let curY = 0;
+let tgX = 0;
+let tgY = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
+
+    function move() {
+        curX += (tgX - curX) / 7;
+        curY += (tgY - curY) / 7;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
